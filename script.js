c504e39b1cd6b612d4ef205c5b172548387a4f28
STORMTR00P3R <noorblum16@gmail.com>
2023-05-16 18:51:00 -0700

    transferfromrepl

---
+++ script.js
@@ -0,0 +1,5 @@
+let passwordsAmount = document.querySelector('#passwordsAmount');
+let passwordLength  = document.querySelector('#passwordLength');
+let passwordList    = document.querySelector('#passwordList');
+
+let characters = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-={}][\|';:,./<>?'~";
